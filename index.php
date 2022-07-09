d3d7ca0c6fff6af20139e1c1ebd4ac8207c44cac
Saikat141 <72371927+Saikat141@users.noreply.github.com>
2022-07-09 17:07:46 +0600

    added text

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+//session_start();
 
 
 //include("connection.php");
@@ -459,7 +459,7 @@ ul li a:hover{text-decoration:none;}
 	        </div>
 	        <div class="container">
 	        <!--paragraph content-->
-	             <p style="font-family: 'Lobster', cursive; font-weight:light; font-size:25px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	             <p style="font-family: 'Lobster', cursive; font-weight:light; font-size:25px;">Food Items</p>
 	        </div>
 	  </div>
 	   <!--main row 2 left right ends-->
