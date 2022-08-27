f08ae11c29261185ff1c305bf048f2ca22db711e
Saikat141 <72371927+Saikat141@users.noreply.github.com>
2022-08-27 19:55:04 +0600

    cart sign

--- form/cart.php
+++ form/cart.php
@@ -239,7 +239,7 @@ if(isset($del))
 					  <h5 style="color:red;">Grand total</h5>
 					  </td>
 					  <td>
-					  <h5><i class="fas fa-rupee-sign"></i>&nbsp;<?php echo end($gtotal); ?></h5>
+					  <h5><?php echo end($gtotal);?> Tk</h5>
 					  </td>
 					  <td>
 
